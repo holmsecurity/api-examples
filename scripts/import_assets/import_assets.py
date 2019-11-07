f973ca5f7e806661e1fb4e6250d0f3419cc7acb3
hany <hany.iskandar@holmsecurity.com>
2019-11-07 10:59:35 +0100

    applied isort yapf and other cmall changes

--- scripts/import_assets/import_assets.py
+++ scripts/import_assets/import_assets.py
@@ -1,16 +1,20 @@
+import argparse
 import csv
 import json
-import requests
-import argparse
-import os
-from requests.exceptions import HTTPError
 from urllib.parse import urljoin
+import requests
+from requests.exceptions import HTTPError
 
 DEFAULT_API_URL = 'https://se-api.holmsecurity.com/v1/'
+"""
+This code importing csv files including information about assets and creates assets in the holm-api endpoint. 
+"""
 
 
-def read_data(args):
-    # reads data from a csv_file
+def read_post_data(args):
+    """
+    The function takes in args from the user and returns either successful post request or a message with the error.
+    """
 
     with open(args.csv_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -22,12 +26,14 @@ def read_data(args):
             details = row[2]
             hosts_personal_data = row[4]
             tags = [t for t in row[3].split('|') if t]
-            dict_fields = {"name": name,
+            dict_fields = {
+                "name": name,
                 "type": asset_type,
                 "business_impact": business_impact,
                 "details": details,
                 "tags": tags,
-                "hosts_personal_data": str_to_bool(hosts_personal_data)}
+                "hosts_personal_data": str_to_bool(hosts_personal_data)
+            }
             if asset_type == 'network':
                 dict_fields.update({"ip_range": ip})
             else:
@@ -40,18 +46,19 @@ def read_data(args):
                 print(f"The asset could not be added because: {errors}")
 
 
-
 def str_to_bool(s):
     if s in ['true', 'True']:
-         return True
+        return True
     elif s in ['false', 'False']:
-         return False
+        return False
     else:
-         raise ValueError
+        raise ValueError
 
 
 def get_asset_type(ip_row):
-    # check the type of the asset
+    """
+    The function takes in ip_row containing ip or ip_range and detemine whether it is a network or host
+    """
 
     if "/" in ip_row:
         asset_type = "network"
@@ -63,7 +70,10 @@ def get_asset_type(ip_row):
 def post_asset_request(args, dict_fields):
     url = urljoin(args.url, 'net-scans/assets')
     json_data = json.dumps(dict_fields)
-    headers = {"Authorization": f"TOKEN {args.key_token}", "Content-Type": "application/json"}
+    headers = {
+        "Authorization": f"TOKEN {args.key_token}",
+        "Content-Type": "application/json"
+    }
     response = requests.post(url=url, data=json_data, headers=headers)
     response.raise_for_status()
     return response
@@ -71,15 +81,24 @@ def post_asset_request(args, dict_fields):
 
 def get_args():
     parser = argparse.ArgumentParser(description='API key of the sc account')
-    parser.add_argument('-p', '--csv-path', help='Path to the CSV file', required=True)
-    parser.add_argument('-k', '--key-token', type=str, help='Token needed to access sc-dev', required=True)
-    parser.add_argument('-u', '--url',
-                        help=f"API URL to use, default is set to {DEFAULT_API_URL}",
-                        default=DEFAULT_API_URL)
+    parser.add_argument('-p',
+                        '--csv-path',
+                        help='Path to the CSV file',
+                        required=True)
+    parser.add_argument('-k',
+                        '--key-token',
+                        type=str,
+                        help='Token needed to access sc-dev',
+                        required=True)
+    parser.add_argument(
+        '-u',
+        '--url',
+        help=f"API URL to use, default is set to {DEFAULT_API_URL}",
+        default=DEFAULT_API_URL)
 
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = get_args()
-    asset_data = read_data(args)
+    asset_data = read_post_data(args)
